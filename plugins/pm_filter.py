66f0e0935959bc008cc23b7d252236c43e6b517b
SM <119601732+Alexbots1@users.noreply.github.com>
2024-10-09 18:20:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -997,10 +997,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ),
             disable_web_page_preview = True
 	)
-    elif query.data == "mydevelopers":
+    #elif query.data == "mydevelopers":
         #await query.answer("Meet the minds behind this bot:\n\n👨‍💻 @JISSHU_BOTS\n👨‍💻 @IM_JISSHU\n👨‍💻 @JOYBOY_ZORO\n👨‍💻 @TG_Anonymous_Official\n\n\n❤️ A big thank you for making this bot awesome!", show_alert=True)
  
-    elif query.data == "Source":
+    #elif query.data == "Source":
         buttons = [[
             #InlineKeyboardButton('Repo', url='https://github.com/Jisshubot/Jisshu-Filter-Bot')
         ],[
