b7e4079a34ee21f38dcd8477ee778cb010852bd3
SM <119601732+Alexbots1@users.noreply.github.com>
2024-10-11 01:04:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -990,19 +990,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
 			InlineKeyboardButton('‼️ ᴅɪꜱᴄʟᴀɪᴍᴇʀ ‼️', callback_data='disclaimer')
 		],[
 			#InlineKeyboardButton('Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', callback_data='Source')
-               # ],[
-                        #InlineKeyboardButton('My Developers 😎',callback_data='mydevelopers')
-		#],[
-			#InlineKeyboardButton('« ʜᴏᴍᴇ', callback_data='start')]]
+                ],[
+                       # InlineKeyboardButton('My Developers 😎',callback_data='mydevelopers')
+		],[
+			InlineKeyboardButton('« ʜᴏᴍᴇ', callback_data='start')]]
                 ),
-            #disable_web_page_preview = True
-	#)
-    #elif query.data == "mydevelopers":
+            disable_web_page_preview = True
+	)
+    elif query.data == "mydevelopers":
         #await query.answer("Meet the minds behind this bot:\n\n👨‍💻 @JISSHU_BOTS\n👨‍💻 @IM_JISSHU\n👨‍💻 @JOYBOY_ZORO\n👨‍💻 @TG_Anonymous_Official\n\n\n❤️ A big thank you for making this bot awesome!", show_alert=True)
  
-    #elif query.data == "Source":
+    elif query.data == "Source":
         buttons = [[
-            #InlineKeyboardButton('Repo', url='https://github.com/Jisshubot/Jisshu-Filter-Bot')
+           # InlineKeyboardButton('Repo', url='https://github.com/Jisshubot/Jisshu-Filter-Bot')
         ],[
             InlineKeyboardButton('« ʙᴀᴄᴋ', callback_data='about'),
             InlineKeyboardButton('× ᴄʟᴏꜱᴇ ×', callback_data='close_data')
